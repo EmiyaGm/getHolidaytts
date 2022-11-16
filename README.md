6b9726440b79f5ed3bc1dba3fc48ee49593e9cf3
EmiyaGm <464723943@qq.com>
2022-11-16 14:47:13 +0800

    fix: epidemicdata and readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # getHolidaytts
 获取节假日，休息日提示文本
+
+获取国内、指定省市疫情信息
+
+
+# 使用
+
+```javascript
+npm install
+
+npm run prd
+```
+
+## 访问服务接口地址
+
+获取节假日，休息日提示文本接口 `/getholidaytss`
+
+```javascript
+// 请求地址
+http://127.0.0.1:3003/getholidaytss
+
+// 示例数据
+{
+    "code": 0,
+    "tts": "今天是星期三，还有3天就是周六了，加油吧，打工人！"
+}
+```
+
+获取国内、指定省市疫情信息接口 `/getholidaytss`
+
+```javascript
+// 请求地址
+http://127.0.0.1:3003/getepidemicdata
+
+// 示例数据
+{
+    "code": 0,
+    "data": "2022-11-16 09:14:28 中国现有确诊8792321，较昨日新增40010"
+}
+
+// 请求地址
+http://127.0.0.1:3003/getepidemicdata?city_name=南京
+
+// 示例数据
+{
+    "code": 0,
+    "data": "2022-11-16 09:38:29 南京现有确诊619，较昨日新增4"
+}
+```
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const { getHolidaytts, getEpidemicData } = require('../services')
-
+const moment = require('moment')
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
@@ -23,7 +23,6 @@ router.get('/getholidaytss', async (ctx, next) => {
   ctx.body = {
     code: 0,
     tts: result,
-    date: today
   }
 })
 
@@ -33,6 +32,7 @@ router.get('/getepidemicdata', async (ctx, next) => {
   let req_query = request.query
   let cities_data = []
   let city_data = {}
+  const check_time = result.timestamp
   if (req_query.city_name) {
     const city_name = req_query.city_name
     if (result.data && result.data.areaTree) {
@@ -49,15 +49,27 @@ router.get('/getepidemicdata', async (ctx, next) => {
       if (city.children) {
         city.children.forEach((child) => {
           if (child.name === city_name) {
-            city_data === child
+            city_data = child
           }
         })
       }
     })
+  } else if (result.data && result.data.areaTree) {
+    result.data.areaTree.forEach((area) => {
+      if (area.name === '中国') {
+        city_data = area
+      }
+    })
+  }
+  let data = null
+  if (Object.keys(city_data).length === 0) {
+    data = `${moment(check_time).format('YYYY-MM-DD HH:mm:ss')} ${req_query.city_name || ''}暂无疫情数据`
+  } else {
+    data = `${city_data.lastUpdateTime} ${req_query.city_name || '中国'}现有确诊${city_data.total.confirm}，较昨日新增${city_data.today.confirm}`
   }
   ctx.body = {
     code: 0,
-    city_data
+    data
   }
 })
 
